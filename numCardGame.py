a35274fa955ef213da9d09cb0c2d573b13592469
신현수 <69250097+ShinHyun-soo@users.noreply.github.com>
2022-03-12 19:27:18 +0900

    Update numCardGame.py

--- numCardGame.py
+++ numCardGame.py
@@ -22,7 +22,7 @@ for i in range(n):
 
   min_value = 10001
   for a in data:
-    min_value = min(min_value, a)       #여기 왜 a가 있지?
+    min_value = min(min_value, a)       #여기 왜 a가 있지? -> data 원소, data가 [5 3 1 5] 면 네번 반복문 돌면서 a = 5 , a = 3, a = 1, a = 5 로 바뀜.(by.meansub)
 
     result = max(result, min_value)
 
